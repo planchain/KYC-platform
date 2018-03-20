e80ee6c27db1ef56476104ca5439c174a54ea5c1
Jurgen Schouppe <jurgen.soltronics@gmail.com>
2018-03-20 11:47:11 +0100

    Update settings.php

--- Source/settings.php
+++ Source/settings.php
@@ -1,11 +1,3 @@
-<?php
-$rate = 2000;
-$packages = array(
-				array("Bronze package",0.5,1),
-				array("Silver package",1,1),
-				array("Gold package",2,1),
-				array("Diamond package",5,1),
-				array("Platinum package",10,1),
-				array("Custom package",0,1)
-			);
+<?php	
+// Server side code has been deleted for security reasons and will be added after crowdsale
 ?>
